399438831b72a9a41e6ca2ccdaa5554c611f0859
AJDelgadillo <105257231+AJDelgadillo@users.noreply.github.com>
2022-10-19 12:55:12 -0700

    Add files via upload
    Updating index.html, with 2 additional html pages and updated static folders

--- static/js/app.js
+++ static/js/app.js
@@ -12,24 +12,60 @@ function selectZip() {
                 .property('value', zip);
         });
     })
-}
+};
 
 selectZip();
 
-function buildCensus(zip){
-    d3.json('static/js/data.json').then((data) => {
-    var zipCode = data.Zip_Code;
+function zipChanged(newZip) {
+    buildDemographics(tableData, newZip);
+}
 
-    var resultArray = zipCode.filter(sampleObj => sampleObj.id == zip)
-    var result = resultArray[0];
+function buildDemographics(data, zip) {
+    console.log(zip)
+    console.log(data[0])
+    var resultArray = data.filter(data => data.Zip_Code == zip)
+    var result = resultArray[0]
+    console.log(result)
+    var PANEL = d3.select("#census-data");
 
-    var PANEL = d3.select('census-data');
-
-    PANEL.html('');
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
 
     Object.entries(result).forEach(([key, value]) => {
-        PANEL.append('h6').text(`${key.toUpperCase()}: ${value}`);
-    })
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      });
+};
 
-    })
-}
+var tbody = d3.select('tbody');
+var thead = d3.select('thead');
+
+const tableData = data
+
+
+// Function to select the table elements on the html file
+function buildTable(data) {
+    thead.html('');
+    tbody.html('');
+
+    thead.append('tr')
+    for (key in data[0]) {
+        thead
+            .append('th')
+            .text(key)
+    }
+
+    tbody.append('tr');
+    for (var i = 0; i < data.length; i++) {
+        tbody.append('tr');
+        for (key in data[i]) {
+          tbody
+            .append('td')
+            .text(data[i][key]);
+      }
+    };
+};
+
+buildTable(tableData)
+
+
+
