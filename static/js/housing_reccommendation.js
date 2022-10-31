d4ab7bf0b63d21555697a707bcd2e0c9f22f7af7
Atros04 <iiiiamtrakiiii@hotmail.com>
2022-10-30 18:19:24 -0700

    Cleaning

--- static/js/housing_reccommendation.js
+++ static/js/housing_reccommendation.js
@@ -1,10 +1,3 @@
-// We create the tile layer that will be the background of our map.
-let street = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
-
 // We create the dark view tile layer that will be an option for our map.
 let satelliteStreet = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,7 +7,6 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: street,
     Satellite: satelliteStreet
 };
 
@@ -22,30 +14,65 @@ let baseMaps = {
 let map = L.map('mapid', {
     center: [32.98, -116.77],
     zoom: 10,
-    layers: [street]
+    layers: [satelliteStreet]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Add GeoJSON data.
-let sandiegoData = "https://raw.githubusercontent.com/tsmtruong/final-project/main/Resources/RAW/San_Diego_Zip_Codes.geojson"
-let classdata = "https://raw.githubusercontent.com/tsmtruong/final-project/ATaylor/Resources/Clean/San_Diego_Housing_Classes.json"
+let sandiegoData = "https://raw.githubusercontent.com/tsmtruong/final-project/ATaylor/Resources/RAW/San_Diego_Zip_Codes.geojson"
+
+// Function that will determine the color of a neighborhood based on the class it belongs to
+function chooseColor(houseclass) {
+    switch (houseclass) {
+    case "0":
+      return "#DDE5B6";
+    case "1":
+      return "#6C584C";
+    case "2":
+      return "#A98467";
+    case "3":
+      return "#f5d49f";
+    case "4":
+      return "#ADC178";
+    default:
+      return "black";
+    }
+}
+
+function chooseOpacity(houseclass) {
+    switch (houseclass) {
+    case "0":
+      return 0.7;
+    case "1":
+      return 0.7;
+    case "2":
+      return 0.7;
+    case "3":
+      return 0.7;
+    case "4":
+      return 0.7;
+    default:
+      return 0;
+    }
+}
 
 // Read JSON Data
 d3.json(sandiegoData).then(function(data) {
     console.log(data);
     //Create GeoJSON layer with retrieved data
     L.geoJSON(data, {
-        color: "black",
-        weight: 1,
-        fillColor: "Yellow",
-        fillOpacity: 0.2,
+        style: function(feature) {
+            return {
+              color: "black",
+              // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+              fillColor: chooseColor(feature.properties.class),
+              fillOpacity: chooseOpacity(feature.properties.class),
+              weight: 1.5
+            };
+          },
         onEachFeature: function(feature, layer) {
-            layer.bindPopup("<h3>Neighborhood: " + feature.properties.community + "<h4> Zip Code: " + feature.properties.zip)}
+            layer.bindPopup("<h3>Neighborhood: " + feature.properties.community + "<h4>Zip Code: " + feature.properties.zip + "<h4>Housing Class: " + feature.properties.class)}
     }).addTo(map)
 });
-
-d3.json(classdata).then(function(data2) {
-    console.log(data2)
-})
